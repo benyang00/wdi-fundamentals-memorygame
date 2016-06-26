82811438c98a30a2e2be83e2e008364673dec7f2
BenYang <benyang00@gmail.com>
2016-06-27 07:57:10 +0800

    added if else statement in main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.");
+}
+else {
+	alert("You have found a match!");
+}
